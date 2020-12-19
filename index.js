adb5f113e106d3ed3cc9cd1b0d72bed2ec8ad6d2
apaskhaver <alexandra.paskhaver@gmail.com>
2020-12-19 13:28:34 -0500

    Access img elements with querySelectorAll instead of by a more circuitous route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var randomNumber = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage = "images/dice" + randomNumber + ".png";
-document.querySelector(".dice").lastElementChild.setAttribute("src", randomDiceImage);
+//get list of all img elements, select the first one (left die) and change it
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
 
 var SecondRandomNumber = Math.floor(Math.random() * 6) + 1;
 randomDiceImage = "images/dice" + SecondRandomNumber + ".png";
-document.querySelector(".container").lastElementChild.lastElementChild.setAttribute("src", randomDiceImage);
+//select second die and change it
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage);
 
 if (randomNumber > SecondRandomNumber) {
   document.querySelector("h1").textContent = "Player 1 wins!";
