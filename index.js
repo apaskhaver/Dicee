340d2537acdeeca630cdbe7db41b75d145ada97f
apaskhaver <alexandra.paskhaver@gmail.com>
2020-12-19 13:25:22 -0500

    Get images by concatenating random number with file name instead of with if statements

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
 var randomNumber = Math.floor(Math.random() * 6) + 1;
-if (randomNumber === 1) {
-  document.querySelector(".dice").lastElementChild.setAttribute("src", "images/dice1.png");
-} else if (randomNumber === 2) {
-  document.querySelector(".dice").lastElementChild.setAttribute("src", "images/dice2.png");
-} else if (randomNumber === 3) {
-  document.querySelector(".dice").lastElementChild.setAttribute("src", "images/dice3.png");
-} else if (randomNumber === 4) {
-  document.querySelector(".dice").lastElementChild.setAttribute("src", "images/dice4.png");
-} else if (randomNumber === 5) {
-  document.querySelector(".dice").lastElementChild.setAttribute("src", "images/dice5.png");
-} else if (randomNumber === 6) {
-  document.querySelector(".dice").lastElementChild.setAttribute("src", "images/dice6.png");
-}
+var randomDiceImage = "images/dice" + randomNumber + ".png";
+document.querySelector(".dice").lastElementChild.setAttribute("src", randomDiceImage);
 
 var SecondRandomNumber = Math.floor(Math.random() * 6) + 1;
-if (SecondRandomNumber === 1) {
-  document.querySelector(".container").lastElementChild.lastElementChild.setAttribute("src", "images/dice1.png");
-} else if (SecondRandomNumber === 2) {
-  document.querySelector(".container").lastElementChild.lastElementChild.setAttribute("src", "images/dice2.png");
-} else if (SecondRandomNumber === 3) {
-  document.querySelector(".container").lastElementChild.lastElementChild.setAttribute("src", "images/dice3.png");
-} else if (SecondRandomNumber === 4) {
-  document.querySelector(".container").lastElementChild.lastElementChild.setAttribute("src", "images/dice4.png");
-} else if (SecondRandomNumber === 5) {
-  document.querySelector(".container").lastElementChild.lastElementChild.setAttribute("src", "images/dice5.png");
-} else if (SecondRandomNumber === 6) {
-  document.querySelector(".container").lastElementChild.lastElementChild.setAttribute("src", "images/dice6.png");
-}
+randomDiceImage = "images/dice" + SecondRandomNumber + ".png";
+document.querySelector(".container").lastElementChild.lastElementChild.setAttribute("src", randomDiceImage);
 
 if (randomNumber > SecondRandomNumber) {
   document.querySelector("h1").textContent = "Player 1 wins!";
